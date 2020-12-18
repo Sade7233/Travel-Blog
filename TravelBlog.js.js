4ecf892290b4b2dcfa385b5b3c3999e6e6f479a2
Sade7233 <73080623+Sade7233@users.noreply.github.com>
2020-12-18 16:02:41 -0500

    Final Edits Pt. I
    Changed Dark Mode color, added borders to all images, used Section to create boxes for each webpage section, styled progress bar, removed underline from headers, added Motivation button at bottom on page, updated Nav bar links, added Footer, rearranged section boxes via margin-left

--- TravelBlog.js.js
+++ TravelBlog.js.js
@@ -1,7 +1,9 @@
 console.log("Hello, it is I");
 //Dark Mode Enabler Button
 var darkModeButton = document.getElementById('DarkMode');
-var body = document.getElementsByTagName("body")[0]
+var body = document.getElementsByTagName("body")[0];
+var krabs = document.getElementById('MrKrabs');
+var bean = document.getElementById('TheBean')
 console.log("darkModeButton");
 
 var darkModeEnabled = false;
@@ -10,15 +12,32 @@ function enableDarkMode () {
   console.log("---Standard Light Mode Enabled", darkModeEnabled)
   if (darkModeEnabled == false) {
     console.log("---Dark Mode Has Been Enabled")
-    body.style.background = "#6C91BF";
+    body.style.background = "#301934";
+    body.style.color = "white";
+    krabs.style.border = "white";
+    bean.style.border = "white";
     darkModeButton.innerHTML="Prefer Light Mode?";
     darkModeEnabled = true;
   }  else { //if darkModeEnabled is false
       console.log("---Dark Mode Has Been Disabled")
       body.style.background = "linear-gradient(to left, #ccccff, #dbf0f9)";
+      body.style.color = "black";
+      body.style.border = "black";
+      krabs.style.border = "black";
+      bean.style.border = "black";
       darkModeButton.innerHTML="Prefer Dark Mode?";
       darkModeEnabled = false;
 
     }
 }
 darkModeButton.addEventListener("click", enableDarkMode);
+
+/*Develop function that reveals Shia LeBouf gif upon user click; gif is otherwise hidden*/
+var motivationButton = document.getElementById('Shia')
+var shiaGif = document.getElementById('ShiaGif')
+
+function Motivation () {
+  document.getElementById("ShiaGif").style.visibility = "visible"
+}
+
+motivationButton.addEventListener("click", Motivation);
